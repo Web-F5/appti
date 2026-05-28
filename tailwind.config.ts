78595c165ff4b7d166927a3d0aab9267d5023e9e
Joshua Ekberg <contact@webf5.au>
2026-05-28 15:49:31 +1000

    using logo colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
         orange: {
           DEFAULT: '#fd9900', //E8845A
           50:  '#FEF0E8',
-          600: '#e6692e', //C4623Aj 
+          600: '#d47209', //,e6692e, C4623Aj 
         },
       },
       fontFamily: {
